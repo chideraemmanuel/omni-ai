e87c0ce157c00f1b3944451a76d30c7968a219e9
chideraemmanuel <120053997+chideraemmanuel@users.noreply.github.com>
2023-10-04 18:06:27 +0100

    feat: lays out login page

---
+++ app/(auth)/login/page.styled.ts
@@ -0,0 +1,100 @@
+import styled from 'styled-components';
+
+export const LoginPageContainer = styled.section`
+  /* background-color: green; */
+  // position: absolute;
+  // inset: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 30px 0;
+`;
+
+export const LoginPageFormContainer = styled.div`
+  /* background-color: blue; */
+  width: min(90%, 600px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const LoginFormHeader = styled.div`
+  text-align: center;
+
+  h2 {
+    padding-bottom: ${({ theme }) => theme.space['fluid-block-space-2']};
+    font-weight: bold;
+    display: inline-block;
+    font-size: ${({ theme }) =>
+      theme.font['md-font'] || 'clamp(1.25rem, 1.14rem + 0.4695vw, 1.5625rem)'};
+
+    @media screen and (max-width: 768px) {
+      font-size: ${({ theme }) =>
+        theme.font['lg-font'] ||
+        'clamp(1.5625rem, 1.3644rem + 0.8451vw, 2.125rem)'};
+    }
+  }
+
+  p {
+    padding-bottom: ${({ theme }) => theme.space['fluid-block-space-2']};
+    /* color: #ebebeb; */
+    color: ${({ theme }) => theme.colors.light['gray-700']};
+    font-size: ${({ theme }) =>
+      theme.font['sm-font'] || 'clamp(0.875rem, 0.831rem + 0.1878vw, 1rem)'};
+
+    a {
+      /* color: var(--primary-color); */
+      color: inherit;
+      text-decoration: underline;
+    }
+  }
+`;
+
+export const LoginForm = styled.div`
+  /* background-color: red; */
+  padding: ${({ theme }) =>
+    `${theme.space['fluid-block-space-5']} ${theme.space['fluid-block-space-5']}`};
+  border-radius: 10px;
+  /* width: min(90%, 600px); */
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.space['fluid-block-space-5']};
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.space['fluid-block-space-2']};
+  }
+`;
+
+export const LoginFormBreak = styled.div`
+  position: relative;
+  width: 100%;
+  display: grid;
+  align-items: center;
+  gap: 5px;
+  grid-template-columns: 1fr auto 1fr;
+
+  span {
+    font-size: ${({ theme }) => theme.font['sm-font']};
+    color: ${({ theme }) => theme.colors.light['gray-700']};
+    /* text-align: center;
+    display: flex;
+    justify-content: center; */
+  }
+
+  div {
+    /* position: absolute; */
+    height: 1px;
+    background-color: ${({ theme }) => theme.colors.light['gray-500']};
+  }
+
+  div:first-child {
+  }
+
+  div:last-child {
+  }
+`;
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,81 @@
+'use client';
+
+import FormInput from '@/components/ui/FormInput/FormInput';
 import { FC } from 'react';
+import {
+  LoginForm,
+  LoginFormBreak,
+  LoginFormHeader,
+  LoginPageContainer,
+  LoginPageFormContainer,
+} from './page.styled';
+import Link from 'next/link';
+import Button from '@/components/ui/button/Button';
+import { FaGoogle } from 'react-icons/fa';
+import { StoreTypes } from '@/redux/store';
+import { useSelector } from 'react-redux';
+import {
+  clearLoginEmailError,
+  clearLoginPasswordError,
+  setLoginEmail,
+  setLoginPassword,
+} from '@/redux/slices/formInputSlice';
 
 interface Props {}
 
 const LoginPage: FC<Props> = () => {
+  const { email, password } = useSelector(
+    (store: StoreTypes) => store.formInputs.login
+  );
+
   return (
-    <>
-      <span>Login Page!</span>
-    </>
+    <LoginPageContainer>
+      <LoginPageFormContainer>
+        <LoginFormHeader>
+          <h2>Login to use OmniAI</h2>
+          <p>
+            Don't have an account? <Link href={'/register'}>Register</Link>
+          </p>
+        </LoginFormHeader>
+
+        <LoginForm>
+          <form>
+            <FormInput
+              type="email"
+              placeholder="Enter your email"
+              label="Email"
+              value={email.value}
+              setValue={setLoginEmail}
+              error={email.error}
+              // error={'this is an error'}
+              clearError={clearLoginEmailError}
+            />
+            <FormInput
+              type="password"
+              placeholder="Pick a password"
+              label="Password"
+              value={password.value}
+              setValue={setLoginPassword}
+              error={password.error}
+              // error={'this is an error'}
+              clearError={clearLoginPasswordError}
+            />
+            <Button tagType="button">Create Account</Button>
+          </form>
+
+          <LoginFormBreak>
+            <div></div>
+            <span>or</span>
+            <div></div>
+          </LoginFormBreak>
+
+          <Button width="100%" variant="google">
+            <FaGoogle />
+            <span>Continue with google</span>
+          </Button>
+        </LoginForm>
+      </LoginPageFormContainer>
+    </LoginPageContainer>
   );
 };
 
