77e92856e42f6f2ab6db71d44b1f144f1be33a0f
chideraemmanuel <120053997+chideraemmanuel@users.noreply.github.com>
2023-10-24 21:09:08 +0100

    chore: adds 404 page

---
+++ app/(auth)/user/password-reset/not-found.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import styles from './not-found.module.scss';
+import Logo from '@/components/ui/logo/Logo';
+import illustration from '@/assets/illustration.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+import Button from '@/components/ui/button/Button';
+
+interface Props {}
+
+const NotFoundPage: FC<Props> = () => {
+  return (
+    <main className={styles.container}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.image}>
+          <Image src={illustration} alt="" />
+        </div>
+
+        <div className={styles.text}>
+          <h2>Page Not Found.</h2>
+          <p>Sorry, we couldn't find the page you're looking for.</p>
+
+          <Button tagType="a" href="/">
+            Back to homepage
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default NotFoundPage;
