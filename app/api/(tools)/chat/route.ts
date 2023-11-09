a583bcaa78b9443ac0aef6064bc5432add666b7a
chideraemmanuel <120053997+chideraemmanuel@users.noreply.github.com>
2023-11-09 01:07:09 +0100

    chore: tools api routes refactor

--- app/api/(tools)/chat/route.ts
+++ app/api/(tools)/chat/route.ts
@@ -14,51 +14,33 @@ export const POST = async (request: NextRequest) => {
   const body = await request.json();
   const messages = body;
 
-  // try {
-  //   const chatCompletion = await openai.chat.completions.create({
-  //     messages: messages,
-  //     model: 'gpt-3.5-turbo',
-  //   });
-
-  //   console.log(chatCompletion.choices);
-
-  //   const responseMessage = chatCompletion.choices[0].message;
-
-  //   return NextResponse.json(responseMessage, { status: 200 });
-  // } catch (error: any) {
-  //   console.log('[CHAT_ERROR]', error);
-  // }
-
   if (!token) {
-    // NO TOKEN
     return NextResponse.json('Not Authorized, no token', { status: 400 });
-  } else {
-    // TOKEN EXISTS
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+  try {
+    console.log('connecting to database...');
+    connectToDatabase();
+    console.log('connected to database.');
 
     try {
-      console.log('connecting to database...');
-      connectToDatabase();
-      console.log('connected to database.');
-
-      // CHECK IF USER WITH ID FROM TOKEN EXISTS
       // @ts-ignore
       const user = await User.exists({ _id: decoded.id });
-      console.log('user', user);
 
       if (!user) {
-        // NO USER WITH THE ID
         return NextResponse.json('Not authorized', { status: 400 });
-      } else if (!messages) {
-        // USER EXISTS BUT NO PROMPT
+      }
+
+      if (!messages) {
         return NextResponse.json(
           { message: 'Please enter a prompt' },
           { status: 400 }
         );
-      } else {
-        // USER EXISTS, AND PROMPT EXISTS
-        console.log('chat compl!');
+      }
 
+      try {
         const chatCompletion = await openai.chat.completions.create({
           messages: messages,
           model: 'gpt-3.5-turbo',
@@ -70,11 +52,26 @@ export const POST = async (request: NextRequest) => {
         const responseMessage = chatCompletion.choices[0].message;
 
         return NextResponse.json(responseMessage, { status: 200 });
+      } catch (error: any) {
+        console.log('[CHAT_COMPLETION_ERROR]', error);
+        return NextResponse.json(
+          { message: 'Internal Server Error' },
+          { status: 500 }
+        );
       }
     } catch (error: any) {
-      console.log('[CHAT_ERROR]', error);
-      return NextResponse.json({ message: 'Server error' }, { status: 500 });
+      console.log('[USER_FETCH_ERROR]', error);
+      return NextResponse.json(
+        { message: 'Internal Server Error' },
+        { status: 500 }
+      );
     }
+  } catch (error: any) {
+    console.log('[DATABASE_CONNECTION_ERROR]', error);
+    return NextResponse.json(
+      { message: 'Internal Server Error' },
+      { status: 500 }
+    );
   }
 };
 
@@ -86,3 +83,22 @@ export const POST = async (request: NextRequest) => {
 //   messages: [{ role: 'user', content: 'Hello' }],
 //   model: 'gpt-3.5-turbo',
 // });
+
+//
+//
+//
+//
+//   // try {
+//   const chatCompletion = await openai.chat.completions.create({
+//     messages: messages,
+//     model: 'gpt-3.5-turbo',
+//   });
+
+//   console.log(chatCompletion.choices);
+
+//   const responseMessage = chatCompletion.choices[0].message;
+
+//   return NextResponse.json(responseMessage, { status: 200 });
+// } catch (error: any) {
+//   console.log('[CHAT_ERROR]', error);
+// }
--- app/api/(tools)/generate-image/route.ts
+++ app/api/(tools)/generate-image/route.ts
@@ -1,38 +1,71 @@
 import { openai } from '@/config/openai';
+import { connectToDatabase } from '@/lib/utils/database';
+import jwt from 'jsonwebtoken';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const POST = async (request: NextRequest) => {
+  const token = request.cookies.get('token')?.value;
   const body = await request.json();
   const { prompt, amount, size } = body;
 
-  console.log(body);
-
-  if (!prompt || !amount || !size) {
-    return NextResponse.json(
-      { error: true, message: 'Please provide the required options' },
-      {
-        status: 400,
-      }
-    );
+  if (!token) {
+    return NextResponse.json('Not Authorized, no token', { status: 400 });
   }
 
-  const imageSize =
-    size === '256' ? '256x256' : size === '512' ? '512x512' : '1024x1024';
+  const decoded = jwt.verify(token, process.env.JWT_SECRET!);
 
   try {
-    const response = await openai.images.generate({
-      prompt,
-      n: parseInt(amount),
-      size: imageSize,
-    });
+    console.log('connecting to database...');
+    connectToDatabase();
+    console.log('connected to database.');
 
-    return NextResponse.json(response.data, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      { error: true, message: 'The image could not be generated' },
-      {
-        status: 400,
+    try {
+      // @ts-ignore
+      const user = await User.exists({ _id: decoded.id });
+
+      if (!user) {
+        return NextResponse.json('Not authorized', { status: 400 });
       }
+
+      if (!prompt || !amount || !size) {
+        return NextResponse.json(
+          { error: true, message: 'Please provide the required options' },
+          {
+            status: 400,
+          }
+        );
+      }
+
+      const imageSize =
+        size === '256' ? '256x256' : size === '512' ? '512x512' : '1024x1024';
+
+      try {
+        const response = await openai.images.generate({
+          prompt,
+          n: parseInt(amount),
+          size: imageSize,
+        });
+
+        return NextResponse.json(response.data, { status: 200 });
+      } catch (error: any) {
+        console.log('[IMAGE_GENERATION_ERROR]', error);
+        return NextResponse.json(
+          { message: 'Internal Server Error' },
+          { status: 500 }
+        );
+      }
+    } catch (error: any) {
+      console.log('[USER_FETCH_ERROR]', error);
+      return NextResponse.json(
+        { message: 'Internal Server Error' },
+        { status: 500 }
+      );
+    }
+  } catch (error: any) {
+    console.log('[DATABASE_CONNECTION_ERROR]', error);
+    return NextResponse.json(
+      { message: 'Internal Server Error' },
+      { status: 500 }
     );
   }
 };
